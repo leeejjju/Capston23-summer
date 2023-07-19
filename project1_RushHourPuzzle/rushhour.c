e63a1e0b3ed73717af6076610b7c6ca2f181a120
leeejjju <dlwlswn02027@gmail.com>
2023-07-19 16:59:03 +0900

    modified some functions...

--- project1_RushHourPuzzle/rushhour.c
+++ project1_RushHourPuzzle/rushhour.c
@@ -48,6 +48,52 @@ int cells[6][6] ; // cells[Y][X]
 /* --- NOFIX */
 
 
+/*
+!TIL
+
+한 개념을 완전히 깔끔하게 끝내고 다음으로. complexity를 계속 줄인다. 헷갈리지 않게!!! 
+본인부터 헷갈리면 남이랑 어케 협업하냐. 
+
+현업에서 실력의 척도 -> 본인의 커밋이 메인 브랜치에 들어가기까지의 기간. 3개월이면 잘한거고 일년이면 짤림.
+본인도 헷갈리는 코드는 코드리뷰도 걸러진다. 
+
+NULL && 하면 false? 
+
+클린코딩 -> 확실하게 코드가 나와야 고쳐쓸수도 있고... 정확한 시스템과 기능을 알아야 이게 가능함. 
+머릿속에 확실하게 알고 있어야 길게 생각할 수 있다. 잘 모르면 상상을 못함 -> 버그터지고 진도 안 나가고 ... 
+
+memset은 비싼 오퍼레이션 -> 중요치 않은 정보는 보안 신경 x ? 
+
+무지성 찍어보기/디버그 대신 로직을 제대로 살필 것. 
+코드를 보고 디버깅하기!!!
+디버거를 켜야 하는 경우: 내가 짜지 않은 부분에서 오는 리턴값을 봐야 할 때 
+
+GDP ? unix기반 라이브러리를 쓸 수 있나? 
+
+
+fuzzing
+load file 테스트
+
+
+main(int argc, char ** argv)
+
+argument로 파일명을 받아서 load_game에 넣기 
+
+afl-gcc: fuzzing의 오픈소스 기본 도구 
+afl-fuzz i input/ -o output/ ./a.out @@(랜덤파일) 
+인풋폴더에 있는 파일들을 베이스로 랜덤값들을 만듬. 
+
+
+path branch의 시퀀스? 경우의 수?
+
+리빌드 후 output폴더에 저장되는 파일을 인풋으로 돌리면 fault가 듬. 크래시.
+cat하면 내용 읽어볼 수 있음. 
+
+대부분의 보안 결함은 입력 파싱하는 부분. 
+
+*/
+
+
 // return the corresponding number for the command given as s.
 commands get_op_code (char * s){
 	int i;
@@ -81,7 +127,7 @@ int load_game (char * filename){
 	
 	for(int k = 0; k < strlen(line); k++){
 		if(!isdigit(line[k])){
-			fprintf(stderr, "[error] invalid type of n_cars\n");
+			fprintf(stderr, "[error] invalid type of n_cars : %c\n", line[k]);
 			fclose(fp);
 			return 1;
 		}
@@ -89,7 +135,7 @@ int load_game (char * filename){
 
 	n_cars = atoi(line);
 	
-	if(!(n_cars >= 2 && n_cars <= 35)){
+	if((n_cars < 2) || (n_cars > 35)){
 		fprintf(stderr, "[error] invalid range of n_cars\n");
 		return 1;
 	}
@@ -100,13 +146,24 @@ int load_game (char * filename){
 	//get the information about each cars
 	while(getline(&line, &len, fp) != -1){
 		line[strlen(line)-1] = 0;
+		char tmp[20];
 		#ifdef DEBUG
 		printf("[debug] car%d : %s\n", i+1, line);
 		#endif 
 
 		strcpy(cell, strtok(line, ":"));
 		strcpy(direction, strtok(NULL, ":"));
-		span = *(strtok(NULL, ":")) - '0';
+		strcpy(tmp, strtok(NULL, ":"));
+		
+		if(cell == NULL || direction == NULL || tmp == NULL){
+			fprintf(stderr, "[error] invalid file format\n");
+			return 1;
+		}
+		if(isdigit(span)){
+			fprintf(stderr, "[error] invalid file format\n");
+			return 1;
+		}
+		span = *(tmp) - '0';
 		cars[i].id = i+1;
 
 		//check the position of c1 (red car)
@@ -118,7 +175,7 @@ int load_game (char * filename){
 
 
 		//check the pivot
-		if(!(cell[0] >= 'A' && cell[0] <= 'F') || !(cell[1] >= '1' && cell[1] <= '6')){
+		if((strlen(cell) != 2) || !(cell[0] >= 'A' && cell[0] <= 'F') || !(cell[1] >= '1' && cell[1] <= '6')){
 			fprintf(stderr, "[error] invalid cell value\n");
 			fclose(fp);
 			return 1;
@@ -166,7 +223,7 @@ int load_game (char * filename){
 		printf("[debug] token : %d:%d, %d:%d, %d, %d \n", cars[i].y1, cars[i].x1, cars[i].y2, cars[i].x2, cars[i].dir, cars[i].span);
 		#endif 
 
-		if(i++ > n_cars){
+		if(++i > n_cars){
 			fprintf(stderr, "[error] wrong num of cars\n");
 			fclose(fp);
 			return 1;
@@ -190,17 +247,17 @@ void display (){
 	 + + + + + 4
 	 + + + + + 4
 	*/
-	printf(" \\ ");
+	printf(" \\  ");
 	for(int i = 0; i < 6; i++){
 		printf("%c ", 'A'+i);
 	}
 	printf("\n");
 
 	for(int y = 5; y >= 0; y--){
-		printf("%d: ", y+1);
+		printf("%-2d: ", y+1);
 		for(int x = 0; x < 6; x++){
-			if(cells[y][x]) printf("%d ", cells[y][x]); 
-			else printf("+ ");
+			if(cells[y][x]) printf("%-2d ", cells[y][x]); 
+			else printf("+  ");
 		}
 		printf("\n");
 	}
@@ -209,11 +266,27 @@ void display (){
 
 
 
+// return 1 if input y,x is already used by other cars
+int isOccupied(int index, int x, int y){
+
+	for(int i = 0; i < index; i++){
+		if(cars[i].dir == vertical){
+			for(int k = 0; k < cars[i].span; k++){
+				if((cars[i].y1 - k == y) && (cars[i].x1 == x)) return 1;
+			} 
+		}else if(cars[i].dir == horizontal){
+			for(int k = 0; k < cars[i].span; k++){
+				if((cars[i].y1 == y) && (cars[i].x1 + k == x)) return 1;
+			}
+		}
+	}
+}
+
+
 // Update cars[id].x1, cars[id].x2, cars[id].y1 and cars[id].y2 according to the given command (op) if it is possible.
 // move returns 1 when the given input is invalid / return 0 for a success.
 // TODO car관련만 다루도록
 //TODO car(모델) -> cell(뷰)로 이어지는 흐름. view의 보장이 없으므로 확실한 분리 - 함수와 관련 없는 변수를 고려할 필요 없도록
-		
 int move (int id, int op){
 	int index = id - 1;  //한 개념이 여러 용도로 쓰이면 안 됨
 
@@ -303,13 +376,13 @@ int update_cells (){
 
 	for(int i = 0; i < n_cars; i++){
 
-		if(cars[i].dir == 0){
+		if(cars[i].dir == vertical){
 			for(int k = 0; k < cars[i].span; k++){
 				if(!cells[cars[i].y1 - k][cars[i].x1]) cells[cars[i].y1 - k][cars[i].x1] = cars[i].id;
 				else  return 1;
 			} 
 
-		}else if(cars[i].dir == 1){
+		}else if(cars[i].dir == horizontal){
 			for(int k = 0; k < cars[i].span; k++){
 				if(!cells[cars[i].y1][cars[i].x1 + k]) cells[cars[i].y1][cars[i].x1 + k] = cars[i].id;
 				else return 1;
@@ -326,7 +399,6 @@ void success(){
 	printf("\n*..☆'.｡.:*.SUCCESS･*..☆.'｡.:*.\n\n");
 	display();
 	printf(".｡.:.+*:ﾟ+｡.ﾟ･*..☆.｡.:*.☆.｡.:'\n\n");
-	free(cars);
 	exit(EXIT_SUCCESS);
 }
 
@@ -336,13 +408,12 @@ int main (){
 	int id ;
 
 	// //! for test
-	// strcpy(buf, "board2.txt");
+	// strcpy(buf, "error.txt");
 	// #ifdef DEBUG
 	// printf("[debug] start the game with file %s\n", buf);
 	// #endif
 	// load_game(buf) ;
 	// update_cells() ;
-	// display();
 	// //! for test
 
 	while (1) {
@@ -362,6 +433,7 @@ int main (){
 					printf("[RushHourPuzzle] invalid file\n");
 					continue;
 				}
+
 				if(update_cells()){
 					printf("[RushHourPuzzle] invalid file\n");
 					fprintf(stderr, "[error] duplicated cars on cells\n");
@@ -381,10 +453,10 @@ int main (){
 				}
 
 				//get the number i 
-				scanf("%d", &id) ;
+				scanf("%s", buf) ;
 				getchar();
 
-				if(id < 1 || id > n_cars){
+				if(!sscanf(buf, "%d", &id) || id < 1 || id > n_cars){
 					printf("[RushHourPuzzle] invalid car number\n");
 					continue;
 				}
@@ -401,11 +473,10 @@ int main (){
 				break;
 			case quit:
 				printf("[RushHourPuzzle] game terminated\n");
-				free(cars);
 				return 0;
 			default:
 				printf("[RushHourPuzzle] invalid command\n");
-				getchar();
+				// getchar();
 
 		}
 
